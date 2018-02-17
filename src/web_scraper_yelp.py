1b5cd0c69b992ceb50f807525fa295e99f199d4a
tensor92 <rbparikh@andrew.cmu.edu>
2018-02-17 15:03:16 -0500

    Updated Yelp files

--- src/web_scraper_yelp.py
+++ src/web_scraper_yelp.py
@@ -13,8 +13,11 @@ def yelp():
     params = {"location": "Pittsburgh", "categories" : "restaurants"}
     response = requests.get('https://api.yelp.com/v3/businesses/search', headers=headers, params=params)
     result = response.json() 
-    content = 'Name,Rating,Review_Count,Price,Cuisine,Latitude,Longitude,Street,City,Zip'
+    content = ['Name','Rating','Review_Count','Price','Cuisine','Latitude','Longitude','Address']
     total = result['total']
+    ofile  = open('links_yelp.csv', "w", encoding='utf-8', newline='')
+    wr = csv.writer(ofile, quoting=csv.QUOTE_ALL)
+    wr.writerow(content)
     for i in range(0, total, 20):
         time.sleep(0.25)
         params = {"location": "Pittsburgh", "categories" : "restaurants", "offset" : i}
@@ -22,16 +25,21 @@ def yelp():
         result = response.json()
         if 'businesses' in result:
             for restaurant in result['businesses']:
-                content += '\n' + restaurant['name'] + ',' + str(restaurant['rating']) + ',' + str(restaurant['review_count']) + ','
+                content = []
+                content.append(restaurant['name']) 
+                content.append(str(restaurant['rating']))
+                content.append(str(restaurant['review_count']))
                 if 'price' not in restaurant:
                     price = ''
                 else:
                     price = restaurant['price']
-                content +=  price + ',' + restaurant['categories'][0]['title'] + ',' + str(restaurant['coordinates']['latitude']) + ',' + str(restaurant['coordinates']['longitude']) + ','+ ','.join(restaurant['location']['display_address'])
-    # Writing to CSV file
-    f = open(os.path.join("yelp.csv"), 'w', encoding='utf8' )
-    f.write(content)
-    f.close()
+                content.append(price)
+                content.append(restaurant['categories'][0]['title'])
+                content.append(str(restaurant['coordinates']['latitude']))
+                content.append(str(restaurant['coordinates']['longitude']))
+                content.append(','.join(restaurant['location']['display_address']))
+                wr.writerow(content)
+
 
 # main driver
 if __name__ == "__main__":
